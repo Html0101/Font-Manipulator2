8fe1a38284bf734689c07c68365340201926abd4
Html0101 <102829836+Html0101@users.noreply.github.com>
2023-08-16 18:04:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+setup()  
+{ 
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() 
+{ 
+    console.log("Model Loaded!");
+}
+
+function gotPoses(results) 
+{ 
+    
+}
+
+draw() 
+{ 
+
+}
+preload()  
+{ 
+
+}
